acdd20a97acc3ea6a68760c40cf7dcdddeec902d
AngelManson <aom@bu.edu>
2026-05-03 12:11:16 -0400

    changed migration handling so we dont just delete everytime

--- app/src/main/java/com/example/rewindjournal/data/JournalDatabase.kt
+++ app/src/main/java/com/example/rewindjournal/data/JournalDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 //@Database(entities = [Folder::class, JournalEntry::class], version = 1, exportSchema = false)
-@Database(entities = [Folder::class, JournalEntry::class], version = 2)
+@Database(entities = [Folder::class, JournalEntry::class], version = 2, exportSchema = true)
 abstract class JournalDatabase : RoomDatabase() {
     abstract fun journalDao(): JournalDao
 
@@ -16,10 +18,27 @@ abstract class JournalDatabase : RoomDatabase() {
         @Volatile
         private var Instance: JournalDatabase? = null
 
+        /*
+         * Manual Migration from 1 -> 2.
+         * Required because version 1 didn't export its schema.
+         * For version 3 and beyond automigration should be used
+         */
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                // Add new columns to 'folders' table
+                db.execSQL("ALTER TABLE folders ADD COLUMN description TEXT NOT NULL DEFAULT ''")
+                db.execSQL("ALTER TABLE folders ADD COLUMN color INTEGER NOT NULL DEFAULT -10354450")
+
+                // Add new columns to 'journal_entries' table
+                db.execSQL("ALTER TABLE journal_entries ADD COLUMN latitude REAL")
+                db.execSQL("ALTER TABLE journal_entries ADD COLUMN longitude REAL")
+            }
+        }
+
         fun getDatabase(context: Context): JournalDatabase {
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, JournalDatabase::class.java, "journal_database")
-                    .fallbackToDestructiveMigration()
+                    .addMigrations(MIGRATION_1_2)
                     .build()
                     .also { Instance = it }
             }
